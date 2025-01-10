5b45514349c0971c7e2eacef35630641c5c57653
Nadi25 <nadine-arzt@gmx.de>
2025-01-10 17:28:33 +0100

    fix seed and date problem

--- RangeX_data_paper_cleaning_phenology_2023.R
+++ RangeX_data_paper_cleaning_phenology_2023.R
@@ -11,17 +11,12 @@
 
 # load library ------------------------------------------------------------
 
-#library(tidyr)
-library(dplyr)
-library(readxl)
-library(readr)
 library(conflicted)
-library(janitor)
-library(lubridate)
-library(here)
-
 conflict_prefer_all("dplyr", quiet = TRUE)
-
+library(tidyverse)
+library(readxl)
+library(janitor)
+library(here)
 
 # Import data low + high --------------------------------------------------
 ## use read_delim from reader package
@@ -34,7 +29,6 @@ phenology_high_raw <- read_excel(here("Data/Data_phenology/RangeX_raw_phenology_
 head(phenology_high_raw)
 dput(colnames(phenology_high_raw))
 glimpse(phenology_high_raw)
-
 
 
 # combine low and high site -----------------------------------------------
@@ -56,7 +50,7 @@ rangex_phenology_raw <- clean_names(rangex_phenology_raw)
 colnames(rangex_phenology_raw)
 
 ## rename column names to match with RangeX metadata file
-rangex_phenology_raw <- rangex_phenology_raw %>%
+rangex_phenology_raw <- rangex_phenology_raw |> 
   rename("block_id_original" = "block_id",
          "plot_id_original" = "plot_id",
          "position_id_original" = "position_id")
@@ -81,7 +75,7 @@ colnames(metadata)
 metadata <- clean_names(metadata)
 
 ## filter only NOR
-metadata_NOR <- metadata %>%
+metadata_NOR <- metadata |> 
   dplyr::filter(grepl('NOR', region))
 head(metadata_NOR)
 str(metadata_NOR)
@@ -105,13 +99,97 @@ colnames(rangex_phenology_raw)
 # 2. Cennig: can have more seeds collected then infructescences, because one stem has multiple flowers
 # 
 # 3. Seeds collected:
-#   --> When seeds have been collected, they obviously can't be counted as infructescences anymore next time.
+# --> When seeds have been collected, they obviously can't be counted as infructescences anymore next time.
 # --> So we have to add the number of collected seeds to the number of infructescences for the next date.
 # --> If 3 seeds have been collected on 27.09.23, we have to add 3 to infructescences on 10.10.23
 # --> Can we specify that when the last seed has been collected, the next date does not get more seeds added to the infructescences? Because then the plant is completely done
 # 
-# 4. Sometimes we have several data points for e.g. buds very close, when not all plots have been observed at the same date
+# 4. Date: Sometimes we have several data points for e.g. buds very close, when not all plots have been observed at the same date
 # --> create new date column with same date for one block of observations
+
+
+
+# Fix seeds collected -----------------------------------------------------
+# Arrange the data by unique plant ID and date
+rangex_phenology_clean <- rangex_phenology_raw |> 
+  arrange(unique_plant_id, date) |> 
+  group_by(unique_plant_id)  |> 
+  mutate(
+    # Use cumsum to sum up seeds collected in previous dates
+    cumsum_seeds_collected = cumsum(seeds_collected),
+    # Adjust the number of infructescences by adding the cumsum of seeds collected
+    number_infructescences_adj = number_infructescences + lag(cumsum_seeds_collected, default = 0)
+  ) |> 
+  ungroup()
+
+## rename number infructescences and delete old column
+rangex_phenology_clean <- rangex_phenology_clean |> 
+  rename("number_infructescences_old" = "number_infructescences",
+         "number_infructescences" = "number_infructescences_adj")
+
+## delete number_infructescences_old, cumsum_seeds_colleted
+rangex_phenology_clean <- rangex_phenology_clean |> 
+  select(-c(number_infructescences_old, cumsum_seeds_collected))
+
+
+
+# Fix date ----------------------------------------------------------------
+## change date name to date_old
+rangex_phenology_clean <- rangex_phenology_clean %>%
+  rename("date_old" = "date")
+
+## count how many measurements per day to see which date needs fixing
+count <- count(rangex_phenology_clean, date_old, site)
+count
+
+## these dates need fixing
+## low site: 11.09 and 13.09 need to be together --> 13.09.2023
+## high site: 15./17./18./19.07 --> 18.07.2023
+## hi: 17./18.08 --> 18.08.2023
+## hi: 29./30.08 --> 29.08.2023
+
+rangex_phenology_clean <- rangex_phenology_clean |> 
+  mutate(date = case_when(
+    # For the low site where date is 2023-09-11, change it to 2023-09-13
+    site == "lo" & date_old == "2023-09-11" ~ as.Date("2023-09-13"),
+    # For the high site where date is 15./17./18./19.07, change it to 18.07.2023
+    site == "hi" & date_old %in% c("2023-07-15", "2023-07-17", "2023-07-19") ~ as.Date("2023-07-18"),
+    # For the high site where date is 17./18.08, change it to 18.08.2023
+    site == "hi" & date_old == "2023-08-17" ~ as.Date("2023-08-18"),
+    # For the high site where date is 29./30.08, change it to 29.08.2023
+    site == "hi" & date_old == "2023-08-30" ~ as.Date("2023-08-29"),
+    # For all other cases, keep the original date
+    .default = date_old
+  ))
+
+count_new <- count(rangex_phenology_clean, date, site)
+count_new
+
+## delete date_old
+rangex_phenology_clean <- rangex_phenology_clean |> 
+  select(-date_old)
+
+
+
+# Fix Centaurea problem ---------------------------------------------------
+
+# Import demographic traits low and high 23 -------------------------------
+
+demographic_traits_23 <- read.csv("Data/Data_demographic_traits/RangeX_clean_traits_2023.csv")
+
+# Merge phenology with demographic traits ---------------------------------
+# rename unique_plant_ID to match
+rangex_phenology_clean <- rangex_phenology_clean |> 
+  rename("unique_plant_ID" = "unique_plant_id")
+
+rangex_phenology_demo_traits <- left_join(rangex_phenology_clean, demographic_traits_23,
+                                                by = c("unique_plant_ID", "species"))
+
+
+
+
+
+
 
 
 
