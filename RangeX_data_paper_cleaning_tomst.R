c76bbe41de957c0bfa1cf7cbe9f3b2fef5045c22
Nadi25 <nadine-arzt@gmx.de>
2025-03-03 22:33:07 +0100

    filter by site and field season

--- RangeX_data_paper_cleaning_tomst.R
+++ RangeX_data_paper_cleaning_tomst.R
@@ -12,15 +12,16 @@ library(conflicted)
 conflict_prefer_all("dplyr", quiet = TRUE)
 library(tidyverse)
 library(openxlsx)
+library(janitor)
+library(lubridate)
 
-library(readr)
-library(purrr)
-library(myClim)
-
+# comments ----------------------------------------------------------------
+# 2023: high out: "08.06.2023" "20.06.2023" - "23.10.2023"
+# 2023: low out: "12.05.2023" "19.06.2023" - "24.10.2023"
+# so take the later one each?
 
 
 # import data 2023 --------------------------------------------------------
-
 # List all files in the 'Data_tomst_loggers' folder that start with 'data'
 tomst_23 <- list.files(path = "Data/Data_tomst_loggers/tomst_2023/", pattern = "^data_\\d+.*\\.csv$", full.names = TRUE)
 
@@ -60,18 +61,21 @@ head(tomst_data_23)
 
 plot_codes_23 <- read.xlsx("Data/Data_tomst_loggers/tomst_plot_codes_2023.xlsx", sheet = 1, colNames = FALSE)
 
+plot_codes_23 <- read.csv2("Data/Data_tomst_loggers/tomst_plot_codes_2023.csv", header =  FALSE)
+
 plot_codes_23
 
 # split dataset into low and high 
 # high
 plot_high <- plot_codes_23 |> 
-  select(X1, X2, X3, X4, X5)
+  select(V1:V5)
 # low
 plot_low <- plot_codes_23 |> 
-  select(X6, X7, X8, X9, X10)
+  select(V7:V11)
 
 plot_low <- plot_low |> 
-  drop_na(X6)
+  filter(if_any(everything(), ~ !is.na(.) & . != ""))
+
 
 # row 2 as header
 colnames(plot_high) <- plot_high[2, ]
@@ -90,17 +94,82 @@ plot_low <- plot_low |>
 # combine again under each other
 plot_codes_clean <- bind_rows(plot_high, plot_low)
 
+# combine tomst data with plot labels -------------------------------------
+
+tomst_23_raw <- left_join(plot_codes_clean, tomst_data_23, by = tomst)
 
 
 
 
+# Add treat_warming and treat_competition columns based on treat ----------
+tomst_23_raw <- tomst_23_raw |> 
+  mutate(
+    treat_warming = case_when(
+      treat == "A" ~ "warm",
+      treat == "B" ~ "ambi",
+      treat == "C" ~ "warm",
+      treat == "D" ~ "ambi",
+      treat == "E" ~ "warm",
+      treat == "F" ~ "ambi"
+    ),
+    treat_competition = case_when(
+      treat == "A" ~ "vege",
+      treat == "B" ~ "vege",
+      treat == "C" ~ "control",
+      treat == "D" ~ "control",
+      treat == "E" ~ "bare",
+      treat == "F" ~ "bare"
+    )
+  )
 
 
+# delete empty columns -----------------------------------------------------
+tomst_23_raw <- tomst_23_raw |> 
+  select(where(~ !all(is.na(.))))
 
 
+# get temperature data ----------------------------------------------------
+
+# Extract temperature columns into a new data frame
+temperature <- tomst_23_raw %>%
+  select(tomst, date_out, Date, Temp1, Temp2, Temp3, block, treat, treat_warming, treat_competition, site) 
+
+# View the first few rows of the extracted temperature data
+head(temperature)
+summary(temperature)
+
+# fix date
+temperature <- temperature |> 
+  mutate(Date = as.POSIXct(Date, format = "%d.%m.%Y %H:%M:%S"))
+
+# split low and high site -------------------------------------------------
+temp_high <- temperature |> 
+  filter(site == "high") 
+
+temp_low <- temperature |> 
+  filter(site == "low") 
+
+# define time period field season -----------------------------------------
+# high
+start_date <- as.POSIXct("2023-06-21 10:00:00", format = "%Y-%m-%d %H:%M:%S")
+end_date <- as.POSIXct("2023-10-23 10:00:00", format = "%Y-%m-%d %H:%M:%S")
+
+# Filter the data for the specified date range
+filtered_temp_high <- temp_high |> 
+  filter(Date >= start_date & Date <= end_date)
+
+head(filtered_temp_high)
 
 
-
+# Create the plot for Temp1 per logger with the filtered data
+ggplot(high_filtered_temperature, aes(x = Date, y = Temp1, color = tomst)) +
+  geom_line() +
+  labs(title = "Temperature (Temp1) per Logger from 31.05.2024 to 15.10.2024",
+       x = "Date",
+       y = "Temperature (\u00B0C)",
+       color = "Logger ID") +
+  theme_minimal() +
+  theme(legend.position = "right")
 
 
 
