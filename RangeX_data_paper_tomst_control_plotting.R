0a3d066fe3fe0501849a4db53b7e7ec774a722eb
Nadi25 <nadine-arzt@gmx.de>
2025-03-06 21:50:21 +0100

    fix names

--- RangeX_data_paper_tomst_control_plotting.R
+++ RangeX_data_paper_tomst_control_plotting.R
@@ -30,15 +30,30 @@ names(tomst_23_raw_filtered)
 # get temperature data for plotting ----------------------------------------------------
 # Extract temperature columns into a new data frame
 temperature <- tomst_23_raw_filtered |> 
-  select(tomst, date_out, date_time, TMS_T1, TMS_T2, TMS_T3, block_ID_original, plot_ID_original, treat_warming, treat_competition, site) 
+  select(tomst, date_out, date_time, TMS_T1, TMS_T2, TMS_T3, block_ID_original, plot_ID_original, 
+         treat_warming, treat_competition, treat_combined, site) 
 
 # View the first few rows of the extracted temperature data
 head(temperature)
 summary(temperature)
 
-temperature <- temperature |> 
-  mutate(date_time = as.Date(date_time, format = "%d.%m.%Y"))
+# temperature <- temperature |> 
+#   mutate(date_time = as.Date(date_time, format = "%d.%m.%Y"))
 
+
+# plot all treatments low and high ----------------------------------------
+temp_average <- temperature |> 
+  group_by(date_time, treat_combined) |> 
+  summarize(avg_temp_1 = mean(TMS_T1, na.rm = TRUE),
+            avg_temp_2 = mean(TMS_T2, na.rm = TRUE),
+            avg_temp_3 = mean(TMS_T3, na.rm = TRUE),.groups = 'drop')
+head(temp_average)
+
+# plot average temp per treat
+ggplot(temp_average, aes(x = date_time, y = avg_temp_1, color = treat_combined)) +
+  geom_line() +
+  theme_minimal() +
+  theme(legend.position = "right")
 
 # split low and high site -------------------------------------------------
 temp_high <- temperature |> 
@@ -47,7 +62,7 @@ temp_high <- temperature |>
 temp_low <- temperature |> 
   filter(site == "lo") 
 
-# control plotting --------------------------------------------------------
+# control plotting high --------------------------------------------------------
 # temp1 all loggers -------------------------------------------------------
 # Create the plot for Temp1 per logger with the filtered data
 ggplot(temp_high, aes(x = date_time, y = TMS_T1, color = tomst)) +
@@ -59,50 +74,50 @@ ggplot(temp_high, aes(x = date_time, y = TMS_T1, color = tomst)) +
 # 94201723 has impossible temp1 values --> delete above in tomst_23_raw
 
 # temp2 -------------------------------------------------------------------
-ggplot(filtered_temp_high, aes(x = Date, y = Temp2, color = tomst)) +
+ggplot(temp_high, aes(x = date_time, y = TMS_T2, color = tomst)) +
   geom_point() +
   theme_minimal() +
   theme(legend.position = "right")
 
 # temp3 -------------------------------------------------------------------
-ggplot(filtered_temp_high, aes(x = Date, y = Temp3, color = tomst)) +
+ggplot(temp_high, aes(x = date_time, y = TMS_T3, color = tomst)) +
   geom_point() +
   theme_minimal() +
   theme(legend.position = "none")
 
-ggplot(filtered_temp_high, aes(x = Date, y = Temp3, color = tomst)) +
+ggplot(temp_high, aes(x = date_time, y = TMS_T3, color = tomst)) +
   geom_line() +
   theme_minimal() +
   theme(legend.position = "none")
 
 
 # Group by treatment wamr, ambi and calculate average temperature --------------------
-temp_high_average <- filtered_temp_high |> 
-  group_by(Date, treat) |> 
-  summarize(avg_temp_1 = mean(Temp1, na.rm = TRUE),
-            avg_temp_2 = mean(Temp2, na.rm = TRUE),
-            avg_temp_3 = mean(Temp3, na.rm = TRUE),.groups = 'drop')
+temp_high_average <- temp_high |> 
+  group_by(date_time, treat_combined) |> 
+  summarize(avg_temp_1 = mean(TMS_T1, na.rm = TRUE),
+            avg_temp_2 = mean(TMS_T2, na.rm = TRUE),
+            avg_temp_3 = mean(TMS_T3, na.rm = TRUE),.groups = 'drop')
 head(temp_high_average)
 
 # plot average temp per treat
-ggplot(temp_high_average, aes(x = Date, y = avg_temp_1, color = treat)) +
+ggplot(temp_high_average, aes(x = date_time, y = avg_temp_1, color = treat_combined)) +
   geom_line() +
   theme_minimal() +
   theme(legend.position = "right")
 
 ggplot(temp_high_average) +
-  geom_line(aes(x = Date, y = avg_temp_1, color = treat)) +
-  geom_line(aes(x = Date, y = avg_temp_2, color = treat)) +
+  geom_line(aes(x = date_time, y = avg_temp_1, color = treat_combined)) +
+  geom_line(aes(x = date_time, y = avg_temp_2, color = treat_combined)) +
   theme_minimal() +
   theme(legend.position = "right")
 
 
 # average per temp --------------------------------------------------------
-temp_high_avg <- filtered_temp_high |> 
-  group_by(Date) |> 
-  summarize(avg_temp_soil = mean(Temp1, na.rm = TRUE),
-            avg_temp_ground = mean(Temp2, na.rm = TRUE),
-            avg_temp_air = mean(Temp3, na.rm = TRUE),.groups = 'drop')
+temp_high_avg <- temp_high |> 
+  group_by(date_time) |> 
+  summarize(avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+            avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+            avg_temp_air = mean(TMS_T3, na.rm = TRUE),.groups = 'drop')
 head(temp_high_avg)
 
 # pivot longer the data
@@ -111,7 +126,7 @@ temp_high_avg_long <- temp_high_avg |>
                names_to = "measurement_position", 
                values_to = "temperature")
 
-ggplot(temp_high_avg_long, aes(x = Date, y = temperature, color = measurement_position)) +
+ggplot(temp_high_avg_long, aes(x = date_time, y = temperature, color = measurement_position)) +
   geom_line() +
   theme_minimal() +
   labs(color = "measurement_position") 
@@ -125,28 +140,28 @@ ggplot(temp_high_avg_long, aes( x = temperature)) +
   theme_minimal()
 
 # q-q plot
-ggplot(temp_high_avg_OTC_long, aes(sample = temperature, color = treat_warming)) +
+ggplot(temp_high_avg_long, aes(sample = temperature)) +
   stat_qq() +
   stat_qq_line() +
   theme_minimal()
 
 # shapiro wil test
-sample_data <- sample(temp_high_avg_OTC_long$temperature, 5000) 
+sample_data <- sample(temp_high_avg_long$temperature, size = 5000) 
 shapiro.test(sample_data)
 
 # Kolmogorov-Smirnov Test
-ks.test(temp_high_avg_OTC_long$temperature, "pnorm", 
-        mean(temp_high_avg_OTC_long$temperature), 
-        sd(temp_high_avg_OTC_long$temperature))
+ks.test(temp_high_avg_long$temperature, "pnorm", 
+        mean(temp_high_avg_long$temperature), 
+        sd(temp_high_avg_long$temperature))
 
 # so data in hist looks normally distributed but based on the tests it is not
 
 # now test if OTCs work ---------------------------------------------------
-temp_high_avg_OTC <- filtered_temp_high |> 
-  group_by(Date, treat_warming) |> 
-  summarize(avg_temp_soil = mean(Temp1, na.rm = TRUE),
-            avg_temp_ground = mean(Temp2, na.rm = TRUE),
-            avg_temp_air = mean(Temp3, na.rm = TRUE),.groups = 'drop')
+temp_high_avg_OTC <- temp_high |> 
+  group_by(date_time, treat_warming) |> 
+  summarize(avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+            avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+            avg_temp_air = mean(TMS_T3, na.rm = TRUE),.groups = 'drop')
 head(temp_high_avg_OTC)
 
 temp_high_avg_OTC_long <- temp_high_avg_OTC |> 
@@ -154,29 +169,31 @@ temp_high_avg_OTC_long <- temp_high_avg_OTC |>
                names_to = "measurement_position", 
                values_to = "temperature")
 
-ggplot(temp_high_avg_OTC_long, aes(x = Date, y = temperature, color = treat_warming)) +
+ggplot(temp_high_avg_OTC_long, aes(x = date_time, y = temperature, color = treat_warming)) +
   geom_line() +
   theme_minimal() +
-  labs(color = "treat_warming") 
+  labs(color = "treat_warming")+
+  scale_color_manual(values = c("warm" = "pink2", "ambi" = "turquoise3"))
 
 # one plot per temp position
-ggplot(temp_high_avg_OTC_long, aes(x = Date, y = temperature, color = treat_warming)) +
+ggplot(temp_high_avg_OTC_long, aes(x = date_time, y = temperature, color = treat_warming)) +
   geom_line() +
   facet_wrap(~ measurement_position, scales = "free_y") + 
   theme_minimal() +
-  labs(color = "Warming Treatment")
+  labs(color = "Warming Treatment")+
+  scale_color_manual(values = c("warm" = "pink2", "ambi" = "turquoise3"))
 
 
 
 # daily temp --------------------------------------------------------------
 # calculate a mean per day and treat_warming
-temp_daily <- filtered_temp_high |> 
-  mutate(Date = as.Date(Date)) |> # only keeps day, not time
-  group_by(Date, treat_warming) |> 
+temp_daily <- temp_high |> 
+  mutate(date_time = as.Date(date_time)) |> # only keeps day, not time
+  group_by(date_time, treat_warming) |> 
   summarize(
-    avg_temp_soil = mean(Temp1, na.rm = TRUE),
-    avg_temp_ground = mean(Temp2, na.rm = TRUE),
-    avg_temp_air = mean(Temp3, na.rm = TRUE),
+    avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+    avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+    avg_temp_air = mean(TMS_T3, na.rm = TRUE),
     .groups = 'drop'
   ) |> 
   pivot_longer(cols = starts_with("avg_temp"), 
@@ -184,7 +201,7 @@ temp_daily <- filtered_temp_high |>
                values_to = "temperature")
 
 
-ggplot(temp_daily, aes(x = Date, y = temperature, color = treat_warming)) +
+ggplot(temp_daily, aes(x = date_time, y = temperature, color = treat_warming)) +
   geom_line() +
   facet_wrap(~ measurement_position, scales = "free_y") +  # Separate panels for soil, ground, air
   theme_minimal() +
@@ -192,25 +209,25 @@ ggplot(temp_daily, aes(x = Date, y = temperature, color = treat_warming)) +
   labs(color = "Warming treatment", y = "Daily mean temperature")
 
 # calcualte max and min temp per day as well
-temp_daily_high <- filtered_temp_high |> 
-  mutate(Date = as.Date(Date)) |>  # Only keeps day, not time
-  group_by(Date, treat_warming) |> 
+temp_daily_high <- temp_high |> 
+  mutate(date_time = as.Date(date_time)) |>  # Only keeps day, not time
+  group_by(date_time, treat_warming) |> 
   summarize(
-    avg_temp_soil = mean(Temp1, na.rm = TRUE),
-    max_temp_soil = max(Temp1, na.rm = TRUE),
-    min_temp_soil = min(Temp1, na.rm = TRUE),
+    avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+    max_temp_soil = max(TMS_T1, na.rm = TRUE),
+    min_temp_soil = min(TMS_T1, na.rm = TRUE),
     
-    avg_temp_ground = mean(Temp2, na.rm = TRUE),
-    max_temp_ground = max(Temp2, na.rm = TRUE),
-    min_temp_ground = min(Temp2, na.rm = TRUE),
+    avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+    max_temp_ground = max(TMS_T2, na.rm = TRUE),
+    min_temp_ground = min(TMS_T2, na.rm = TRUE),
     
-    avg_temp_air = mean(Temp3, na.rm = TRUE),
-    max_temp_air = max(Temp3, na.rm = TRUE),
-    min_temp_air = min(Temp3, na.rm = TRUE),
+    avg_temp_air = mean(TMS_T3, na.rm = TRUE),
+    max_temp_air = max(TMS_T3, na.rm = TRUE),
+    min_temp_air = min(TMS_T3, na.rm = TRUE),
     
     .groups = 'drop'
   ) |> 
-  pivot_longer(cols = -c(Date, treat_warming),  # Keep Date & treat_warming fixed
+  pivot_longer(cols = -c(date_time, treat_warming),  # Keep Date & treat_warming fixed
                names_to = "measurement_type", 
                values_to = "temperature")
 
@@ -220,7 +237,7 @@ temp_daily_high <- filtered_temp_high |>
 temp_ribbon <- temp_daily_high |> 
   pivot_wider(names_from = measurement_type, values_from = temperature)
 
-ggplot(temp_ribbon, aes(x = Date)) +
+ggplot(temp_ribbon, aes(x = date_time)) +
   geom_ribbon(aes(ymin = min_temp_air, ymax = max_temp_air, fill = "Air"), alpha = 0.2) +
   geom_ribbon(aes(ymin = min_temp_soil, ymax = max_temp_soil, fill = "Soil"), alpha = 0.2) +
   geom_ribbon(aes(ymin = min_temp_ground, ymax = max_temp_ground, fill = "Ground"), alpha = 0.2) +
@@ -275,11 +292,12 @@ sunrise_down <- sunrise_down |>
   )
 head(sunrise_down)
 
-
+# sunrise_down <- sunrise_down |> 
+  rename(date_time = Date)
 
 # combine temp high long otc with sunrise data to get day and night -------
 temp_high_avg_OTC_long_day_night <- temp_high_avg_OTC_long |> 
-  mutate(Date_only = as.Date(Date)) |>   
+  mutate(Date_only = as.Date(date_time)) |>   
   left_join(sunrise_down |> 
               select(Date, Sunrise, Sunset), 
             by = c("Date_only" = "Date")) |> 
@@ -289,7 +307,7 @@ temp_high_avg_OTC_long_day_night <- temp_high_avg_OTC_long |>
     Sunset = ymd_hms(paste(Date_only, Sunset)),
     
     # Define day or night
-    day_night = ifelse(Date >= Sunrise & Date <= Sunset, "day", "night")
+    day_night = ifelse(date_time >= Sunrise & date_time <= Sunset, "day", "night")
   ) |> 
   select(-Date_only)
 
@@ -300,7 +318,7 @@ temp_high_avg_OTC_long_day <- temp_high_avg_OTC_long_day_night |>
 
 
 # plot only day warm ambi all 3 temp --------------------------------------
-ggplot(temp_high_avg_OTC_long_day, aes(x = Date, y = temperature, color = treat_warming)) +
+ggplot(temp_high_avg_OTC_long_day, aes(x = date_time, y = temperature, color = treat_warming)) +
   geom_line() +
   facet_wrap(~ measurement_position, scales = "free_y") +  # Separate panels for soil, ground, air
   theme_minimal() +
@@ -351,49 +369,38 @@ ggplot(temp_high_avg_OTC_long_day, aes(x = treat_warming, y = temperature, fill 
 
 
 # temp low: -----------------------------------------
-# define time period field season -----------------------------------------
-start_date_lo <- as.Date("2023-06-20") 
-end_date_lo <- as.Date("2023-10-23") # were collected on 24.10
-
-# Filter the data for the specified date range
-filtered_temp_low <- temp_low |> 
-  filter(Date >= start_date_lo & Date <= end_date_lo)
-
-head(filtered_temp_low)
-
-
 # control plotting --------------------------------------------------------
 # temp1 all loggers -------------------------------------------------------
 # Create the plot for Temp1 per logger with the filtered data
-ggplot(filtered_temp_low, aes(x = Date, y = Temp1, color = tomst)) +
+ggplot(temp_low, aes(x = date_time, y = TMS_T1, color = tomst)) +
   geom_point() +
   theme_minimal() +
   theme(legend.position = "right")
 
 # temp2 -------------------------------------------------------------------
-ggplot(filtered_temp_low, aes(x = Date, y = Temp2, color = tomst)) +
+ggplot(temp_low, aes(x = date_time, y = TMS_T2, color = tomst)) +
   geom_point() +
   theme_minimal() +
   theme(legend.position = "right")
 
 # temp3 -------------------------------------------------------------------
-ggplot(filtered_temp_low, aes(x = Date, y = Temp3, color = tomst)) +
+ggplot(temp_low, aes(x = date_time, y = TMS_T3, color = tomst)) +
   geom_point() +
   theme_minimal() +
   theme(legend.position = "none")
 
-ggplot(filtered_temp_low, aes(x = Date, y = Temp3, color = tomst)) +
+ggplot(temp_low, aes(x = date_time, y = TMS_T3, color = tomst)) +
   geom_line() +
   theme_minimal() +
   theme(legend.position = "none")
 
 
 # pivot longer the data ---------------------------------------------------
-temp_low_avg <- filtered_temp_low |> 
-  group_by(Date, treat_combined) |> 
-  summarize(avg_temp_soil = mean(Temp1, na.rm = TRUE),
-            avg_temp_ground = mean(Temp2, na.rm = TRUE),
-            avg_temp_air = mean(Temp3, na.rm = TRUE),.groups = 'drop')
+temp_low_avg <- temp_low |> 
+  group_by(date_time, treat_combined) |> 
+  summarize(avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+            avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+            avg_temp_air = mean(TMS_T3, na.rm = TRUE),.groups = 'drop')
 head(temp_low_avg)
 
 temp_low_avg_long <- temp_low_avg |> 
@@ -402,20 +409,20 @@ temp_low_avg_long <- temp_low_avg |>
                values_to = "temperature")
 
 # plot
-ggplot(temp_low_avg_long, aes(x = Date, y = temperature, color = measurement_position)) +
+ggplot(temp_low_avg_long, aes(x = date_time, y = temperature, color = measurement_position)) +
   geom_line() +
   theme_minimal() +
   labs(color = "measurement_position") 
 
 # daily temp --------------------------------------------------------------
 # calculate a mean per day and treat_competition
-temp_daily_low <- filtered_temp_low |> 
-  mutate(Date = as.Date(Date)) |> # only keeps day, not time
-  group_by(Date, treat_combined) |> 
+temp_daily_low <- temp_low |> 
+  mutate(date_time = as.Date(date_time)) |> # only keeps day, not time
+  group_by(date_time, treat_combined) |> 
   summarize(
-    avg_temp_soil = mean(Temp1, na.rm = TRUE),
-    avg_temp_ground = mean(Temp2, na.rm = TRUE),
-    avg_temp_air = mean(Temp3, na.rm = TRUE),
+    avg_temp_soil = mean(TMS_T1, na.rm = TRUE),
+    avg_temp_ground = mean(TMS_T2, na.rm = TRUE),
+    avg_temp_air = mean(TMS_T3, na.rm = TRUE),
     .groups = 'drop'
   ) |> 
   pivot_longer(cols = starts_with("avg_temp"), 
@@ -423,7 +430,7 @@ temp_daily_low <- filtered_temp_low |>
                values_to = "temperature")
 
 
-ggplot(temp_daily_low, aes(x = Date, y = temperature, color = treat_combined)) +
+ggplot(temp_daily_low, aes(x = date_time, y = temperature, color = treat_combined)) +
   geom_line() +
   facet_wrap(~ measurement_position, scales = "free_y") + 
   theme_minimal() +
@@ -431,7 +438,6 @@ ggplot(temp_daily_low, aes(x = Date, y = temperature, color = treat_combined)) +
   labs(color = "treatment combind", y = "Daily mean temperature")
 
 
-# combine filtered low and filtered high and soil moisture ------------------
 
 
 
