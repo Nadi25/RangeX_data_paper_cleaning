8d1d0b12121d2ad35431b08b549cfc4c3bba38c5
Nadi25 <nadine-arzt@gmx.de>
2025-06-12 19:39:32 +0200

    a lot more species name fixes

--- RangeX_data_paper_cleaning_community_species_corrections.R
+++ RangeX_data_paper_cleaning_community_species_corrections.R
@@ -8,13 +8,14 @@
 ##            RangeX_metadata_plot_NOR.csv
 ## Date:      11.06.2025
 ## Author:    Nadine Arzt
-## Purpose:   Cleaning of the complete raw data files of community 2021-2023
+## Purpose:   Cleaning of the species names 2021-2023
 
 
 # check and discuss! ----------------------------------------------------------
 # 2021: is hi 3A and 3B switched? changed to b = NOR.hi.warm.vege.wf.03 and a = NOR.hi.ambi.vege.wf.03
 
 # Circle grass = Festuca pratensis
+# Carex flava = Carex demissa
 
 # ?Galium: NOR.lo.ambi.vege.wf.06: lo 6A, 2021-08-30
 # ?Melica nutans (grass 2 ): NOR.hi.warm.vege.wf.06, hi 6A: 2021-08-13
@@ -26,6 +27,7 @@
 # ?Stellaria graminea at high site?: NOR.hi.warm.vege.nf.06, hi 6C and NOR.hi.ambi.vege.wf.07, hi 7B
 
 # Arve sp: NOR.hi.warm.vege.wf.06, hi 6A
+# maybe Stellaria graminea?
 
 # Avenella flexuosa (Deschampsia): NOR.hi.ambi.vege.wf.01, hi 1B
 # is now called Deschampsia flexuosa
@@ -38,10 +40,24 @@
 
 # Carex bigetowii?: NOR.hi.ambi.vege.nf.03, hi 3D - check 24 and field
 
+# Carex lapponica?? Must be something else: NOR.lo.ambi.vege.wf.05, lo 5A
 
+# Carex sp...: hi 2abc, 3cd, 4a - 2021 - don't know how to figure them out
 
+# Cirsium arvense = C. helenioides = C. heterophyllum
 
+# Euphrasia frigida or stricta? hard to differentiate
 
+# ??Grass 1: NOR.hi.warm.vege.wf.06, hi6A, is it the same as Hairy poa/festuca?
+# 126: Hairy poa/festuca? --> Danthonia decumbens?
+
+# Hairy Leontodon and Hairy Taraxacum/Leontodon: NOR.hi.ambi.vege.nf.08 hi8D 
+# and hi10b NOR.hi.ambi.vege.wf.10
+# hi7a in 23 is hairy leontodon
+# just Leontodon autumnalis?
+# many Leontodon sp in lo site 2,3,4,5
+
+# Omalotheca supinum - Omalotheca sylvatica -  Omalotheca norwegica
 
 # check in the field ------------------------------------------------------
 # hi 8d: ?Rubus chamaemorus, NOR.hi.ambi.vege.nf.08 = Filipendula ulmaria
@@ -54,6 +70,9 @@
 # Bold Ajuga?: NOR.hi.ambi.vege.nf.01, hi 1D
 # probably Ajia pyramidalis without hairs
 
+# Carex palustris: NOR.hi.warm.vege.wf.06, hi 6A
+# potentially Carex pilulifera??
+# not sure!! Ask Dagmar
 
 
 
@@ -120,6 +139,8 @@ community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
 # 6: 	?Melica nutans (grass 2 )
 # NOR.hi.warm.vege.wf.06, hi 6A: 2021-08-13
 # could it be the same as Hairy poa/festuca? in 23?
+# 154: Melica nutans (grass 2): no value
+# maybe just delete?
 
 
 # 7: ?Omalotheca
@@ -240,7 +261,12 @@ community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
 
 # 30: Arve sp: NOR.hi.warm.vege.wf.06
 # don't know what that is
-# delete?
+# maybe Stellaria graminea (23 datasheet for hi 6A)
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Arve sp" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Stellaria graminea",
+    TRUE ~ species
+  ))
 
 # 32: Avenella flexuosa (Deschampsia): 
 # NOR.hi.ambi.vege.wf.01
@@ -314,31 +340,315 @@ community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
     TRUE ~ species
   ))
 
+# 55: Carex demisa: NOR.hi.warm.vege.nf.05
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex demisa" & unique_plot_ID == "NOR.hi.warm.vege.nf.05" ~ "Carex demissa",
+    TRUE ~ species
+  ))
+# NOR.hi.warm.vege.wf.06
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex demisa" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Carex demissa",
+    TRUE ~ species
+  ))
+# NOR.hi.warm.vege.nf.06
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex demisa" & unique_plot_ID == "NOR.hi.warm.vege.nf.06" ~ "Carex demissa",
+    TRUE ~ species
+  ))
 
 
+# NOR.hi.warm.vege.wf.06: Anthoxanthum, subturf 11, 6ff, 2023
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  mutate(`11` = if_else(
+    species == "Anthoxanthum odoratum" & 
+      unique_plot_ID == "NOR.hi.warm.vege.wf.06" & 
+      year == 2023, 
+    "6f", 
+    `11`
+  ))
+
+# all Carex flava is Carex demissa
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex flava" ~ "Carex demissa",
+    TRUE ~ species
+  ))
+
+# 61: Carex lapponica?? Must be something else: NOR.lo.ambi.vege.wf.05
+# 2021 
+# change to Carex sp.?
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex lapponica?? Must be something else" & unique_plot_ID == "NOR.lo.ambi.vege.wf.05" ~ "Carex sp.",
+    TRUE ~ species
+  ))
+
+# 62: Carex leparina: NOR.hi.warm.vege.nf.03
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex leparina Must be something else" & unique_plot_ID == "NOR.lo.ambi.vege.wf.05" ~ "Carex leporina",
+    TRUE ~ species
+  ))
+
+# 67: Carex palllescens: NOR.hi.ambi.vege.wf.09
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex palllescens" & unique_plot_ID == "NOR.hi.ambi.vege.wf.09" ~ "Carex pallescens",
+    TRUE ~ species
+  ))
+
+# 68: Carex palustre: NOR.hi.ambi.vege.wf.04, hi 4B should be Cirsium palustre
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex palustre" & unique_plot_ID == "NOR.hi.ambi.vege.wf.04" ~ "Cirsium palustre",
+    TRUE ~ species
+  ))
+
+# Carex palustre: NOR.hi.warm.vege.wf.04 = Cirsium
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex palustre" & unique_plot_ID == "NOR.hi.warm.vege.wf.04" ~ "Cirsium palustre",
+    TRUE ~ species
+  ))
+
+# Carex palustris: NOR.hi.warm.vege.wf.06 2022
+# potentially Carex pilulifera??
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex palustris" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Carex cf. pilulifera",
+    TRUE ~ species
+  ))
+# Carex palustris?: NOR.hi.warm.vege.wf.06 2023
+# delete
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Carex palustris?" & unique_plot_ID %in% c("NOR.hi.warm.vege.wf.06")))
+
+# 73: Carex piluluifera: NOR.hi.warm.vege.wf.02
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Carex piluluifera" & unique_plot_ID == "NOR.hi.warm.vege.wf.02" ~ "Carex pilulifera",
+    TRUE ~ species
+  ))
+
+# 75: Carex sp
+# 76: Carex sp 2
+# 77: Carex sp 4
+# 78: Carex sp 5
+# no value - delete
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!( (species == "Carex sp" | species == "Carex sp 2"
+             | species == "Carex sp 4" | species == "Carex sp 5")
+             & total_cover == "0"))
+
+# 80: Cearex demissa: NOR.hi.warm.vege.nf.07
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Cearex demissa" & unique_plot_ID == "NOR.hi.warm.vege.nf.07" ~ "Carex demissa",
+    TRUE ~ species
+  ))
 
 
-
-
-
-
-
+# 83: Circium palustre = Cirsium palustre
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Circium palustre" ~ "Cirsium palustre",
+    TRUE ~ species
+  ))
 
 # 84: Circle grass = Festuca pratensis
 community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
   mutate(species = case_when(species == "Circle grass" ~ "Festuca pratensis",
                              TRUE ~ species))
 
+# 86: Cirsium helenoides = Cirsium helenioides
+# C.helenoides is probably a synonym of Cirsium heterophyllum
+# also it was recorded as C. arvense in 21 and 22
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Cirsium helenoides" ~ "Cirsium heterophyllum",
+    species == "Cirsium arvense" ~ "Cirsium heterophyllum",
+    TRUE ~ species
+  ))
+
+# 94: Empitrum nigrum
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Empitrum nigrum" & unique_plot_ID == "NOR.hi.warm.vege.nf.02" ~ "Empetrum nigrum",
+    TRUE ~ species
+  ))
+
+# 96: Equilobium anagallidifolium? cf anagallidifolium, hi5B
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Equilobium anagallidifolium?" ~ "Equilobium cf. anagallidifolium",
+    TRUE ~ species
+  ))
+
+# 100: Equisetum sylvestris = E. sylvaticum
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Equisetum sylvestris" ~ "Equisetum sylvaticum",
+    TRUE ~ species
+  ))
+
+# 101: 	Equisteum pratense; NOR.hi.ambi.vege.nf.09
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Equisteum pratense"
+            & total_cover == "0"))
+
+# 103: Euphrasia fragida: NOR.hi.warm.vege.wf.06
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Euphrasia fragida" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Euphrasia frigida",
+    TRUE ~ species
+  ))
+
+# 104: Euphrasia str
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Euphrasia str" ~ "Euphrasia stricta",
+    TRUE ~ species
+  ))
+
+# 107: Fern
+# delete
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Fern" & total_cover == "0"))
+
+# 108: Festuca fuzzy
+# 109: Festuca fuzzy?
+# Danthonia decumbens found out in 23
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Festuca fuzzy" ~ "Danthonia decumbens",
+    species == "Festuca fuzzy?" ~ "Danthonia decumbens",
+    TRUE ~ species
+  ))
+
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Festuca fuzzy" & total_cover == "0"))
 
 
+# 110: Festuca pratense: NOR.lo.ambi.vege.wf.02 and NOR.lo.ambi.vege.wf.01
+# 113: Fesuca pratensis
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Festuca pratense" ~ "Festuca pratensis",
+    species == "Fesuca pratense" ~ "Festuca pratensis",
+    TRUE ~ species
+  ))
+
+# 115: 	Fluffy grass (hitchhiker?) = Holcus lanatus
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Fluffy grass (hitchhiker?)" ~ "Holcus lanatus",
+    TRUE ~ species
+  ))
+
+# 119: Galium uligonosum = Galium uliginosum in NOR.hi.warm.vege.nf.06
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Galium uligonosum" ~ "Galium uliginosum",
+    TRUE ~ species
+  ))
+
+# 122: Grass 1: NOR.hi.warm.vege.wf.06, hi6A, is it the same as Hairy poa/festuca?
+# 126: Hairy poa/festuca?
+# Danthonia decumbens?
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Grass 1" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Danthonia decumbens",
+    species == "Hairy poa/festuca?" & unique_plot_ID == "NOR.hi.warm.vege.wf.06" ~ "Danthonia decumbens",
+    TRUE ~ species
+  ))
+
+# 123: Grass sp
+# was found as leaf with skitracks and long hairs
+# Danthonia decumbens in 24, so it was that in 23 but not sure about 21
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Grass sp" & unique_plot_ID == "NOR.hi.warm.vege.wf.04" 
+    & year == "2023" ~ "Danthonia decumbens",
+    TRUE ~ species
+  ))
+
+# 124: Hairy Leontodon: NOR.hi.ambi.vege.nf.08 hi8D and hi10b NOR.hi.ambi.vege.wf.10
+# not recorded in 24, so maybe just Leontodon autumnalis
+# there is some hairy leaves but we didn't distinguish hairy from normal 
+
+# 125: Hairy Taraxacum/Leontodon
+
+# 127: Hairy seedling
+# 128: Hairy seedling (hitchhiker?)
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Hairy seedling" & total_cover == "0"))
+
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Hairy seedling (hitchhiker?)" & total_cover == "0"))
+
+# 131: Hieracium pilosella(=Pilosella officinalis)
+# 132: Hieracium pilusella
+# is synonym of Pilosella officinarum 
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Hieracium pilosella(=Pilosella officinalis)" ~ "Pilosella officinarum",
+    species == "Hieracium pilusella" ~ "Pilosella officinarum",
+    species == "Hieracium pilosella" ~ "Pilosella officinarum",
+    species == "Hieracium" ~ "Pilosella officinarum",
+    TRUE ~ species
+  ))
 
 
+# 133: Hieracium sp: no value
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  filter(!(species == "Hieracium sp" & total_cover == "0"))
+
+# 136: Hypericum maculata
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Hypericum maculata" ~ "Hypericum maculatum",
+    TRUE ~ species
+  ))
+
+# 140: Juncus articulata: NOR.hi.warm.vege.nf.06
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Juncus articulata" ~ "Juncus articulatus",
+    TRUE ~ species
+  ))
+
+# 146: 	Leontodon sp: hi7a in 23 is hairy leontodon
+# lo 2a, 3a, 4a, 5a
+# NOR.lo.ambi.vege.wf.04 is Leontodon autumnalis
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |> 
+  mutate(species = case_when(
+    species == "Leontodon sp" & unique_plot_ID == "NOR.lo.ambi.vege.wf.04"
+    ~ "Leontodon autumnalis",
+    species == "Leontodon sp" & unique_plot_ID == "NOR.lo.ambi.vege.wf.05"
+    ~ "Leontodon autumnalis",
+    species == "Leontodon sp" & unique_plot_ID == "NOR.lo.ambi.vege.wf.02"
+    ~ "Leontodon autumnalis",
+    TRUE ~ species
+  ))
 
 
+# Knautia arvensis in NOR.lo.ambi.vege.wf.03 in subturf 6: 1?
+community_data_raw_NOR_fixed <- community_data_raw_NOR_fixed |>
+  mutate(`6` = if_else(
+    species == "Knautia arvensis" & 
+      unique_plot_ID == "NOR.lo.ambi.vege.wf.03" & 
+      year == 2023, 
+    "1", 
+    `6`
+  ))
 
-
-
-
+# 158: Omalotheca supinum is probably sylvatica 
+# but Omalotheca is complicated
+# could be also O. norwegica
+# depends on number of nerves on leaves but hard to see
 
 
 
