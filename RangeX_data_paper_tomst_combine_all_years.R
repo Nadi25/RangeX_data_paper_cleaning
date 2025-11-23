e131c04bf948fc95f29449ba40e6e1d7e85146dd
Nadi25 <nadine-arzt@gmx.de>
2025-11-23 13:30:04 +0100

    create one EnvTMS4 dataset for all years

---
+++ RangeX_data_paper_tomst_combine_all_years.R
@@ -0,0 +1,42 @@
+
+# Climate data TOMST loggers NOR 2021-2024 data combination ------------------------------
+
+## Data used: 
+## Date:      21.11.2025
+## Author:    Nadine Arzt
+## Purpose:   Combine TOMST logger data 2021 - 2024
+
+# load library ------------------------------------------------------------
+library(conflicted)
+conflict_prefer_all("dplyr", quiet = TRUE)
+library(tidyverse)
+
+
+# Import TMS data 2021-24 -----------------------------------------------------
+tms21 <- read.csv("Data/Data_tomst_loggers/CleanEnvTMS4/RangeX_clean_EnvTMS4_2021_NOR.csv")
+tms22 <- read.csv("Data/Data_tomst_loggers/CleanEnvTMS4/RangeX_clean_EnvTMS4_2022_NOR.csv")
+tms23 <- read.csv("Data/Data_tomst_loggers/CleanEnvTMS4/RangeX_clean_EnvTMS4_2023_NOR.csv")
+tms24 <- read.csv("Data/Data_tomst_loggers/CleanEnvTMS4/RangeX_clean_EnvTMS4_2024_NOR.csv")
+
+
+
+# combine all years into one file -----------------------------------------
+EnvTMS4 <- bind_rows(tms21, tms22, tms23, tms24)
+
+
+# save clean data ---------------------------------------------------------
+write.csv(EnvTMS4, "Data/Data_tomst_loggers/CleanEnvTMS4/RangeX_clean_EnvTMS4_2021-24_NOR.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
