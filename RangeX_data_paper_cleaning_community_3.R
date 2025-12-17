43995b95109cc11970b8cac2e0b042fc8b6d973c
Nadi25 <nadine-arzt@gmx.de>
2025-12-17 09:31:59 +0100

    more names

--- RangeX_data_paper_cleaning_community_3.R
+++ RangeX_data_paper_cleaning_community_3.R
@@ -4,7 +4,7 @@
 ## Data used: RangeX_clean_VegSurveyGeneral_21_22_23_NOR.csv
 ## Date:      10.12.2025
 ## Author:    Nadine Arzt
-## Purpose:   Fixing some more things
+## Purpose:   Fixing some more things, also based on decicions from 2025
 
 # load library ------------------------------------------------------------
 library(conflicted)
@@ -13,23 +13,29 @@ library(tidyverse)
 
 
 # import vege data nor general --------------------------------------------
-vege_nor <- read.csv("Data/Data_community/CleanVegSurvey/RangeX_clean_VegSurveyGeneral_21_22_23_NOR.csv")
+vege <- read.csv("Data/Data_community/CleanVegSurvey/RangeX_clean_VegSurveyGeneral_21_22_23_NOR.csv")
 
 
 # fix <1 and X ------------------------------------------------------------
-vege_nor <- vege_nor |>
+vege_nor <- vege |>
   mutate(
     cover = case_when(
       cover == "<1" ~ "0.5",
-      cover == "X"  ~ NA_character_,
+      cover == "X"  ~ "1",
+      cover == "12+R2:R25" ~ "12",
       TRUE ~ cover
     ),
     cover = as.numeric(cover)
   )
 
+# remove rows with 0 in cover ---------------------------------------------
+vege_nor <- vege_nor |>
+  filter(cover != 0)
 
 
-
+# save clean data cover plot ----------------------------------------------
+#write.csv(vege_nor, "Data/Data_community/CleanVegSurvey/RangeX_clean_VegSurveyGeneral_21_22_23_NOR.csv", row.names = FALSE)
+veg_survey_general <- read_csv("Data/Data_community/CleanVegSurvey/RangeX_clean_VegSurveyGeneral_21_22_23_NOR.csv")
 
 
 
